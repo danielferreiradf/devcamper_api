10ce0f1cd667fe17db328c25d7bbda317c826953
danielferreiradf <danielferreiradk@gmail.com>
2019-10-29 07:16:50 -0300

    setup validation errors handler

--- controllers/BootcampsController.js
+++ controllers/BootcampsController.js
@@ -75,9 +75,12 @@ module.exports = {
       );
 
       if (!bootcamp) {
-        return res.status(400).json({
-          success: false
-        });
+        return next(
+          new ErrorResponse(
+            `Bootcamp not found with id of ${req.params.id}`,
+            404
+          )
+        );
       }
 
       res.status(200).json({ success: true, data: bootcamp });
@@ -94,9 +97,12 @@ module.exports = {
       const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
       if (!bootcamp) {
-        return res.status(400).json({
-          success: false
-        });
+        return next(
+          new ErrorResponse(
+            `Bootcamp not found with id of ${req.params.id}`,
+            404
+          )
+        );
       }
 
       res.status(200).json({ success: true, data: {} });
