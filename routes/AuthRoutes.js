86ed283533270bfdcbf4e5ab4aca6e9bc2311688
danielferreiradf <danielferreiradk@gmail.com>
2019-11-18 13:41:40 -0300

    added user update details and update password

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -4,7 +4,9 @@ const {
   loginUser,
   getLoggedUser,
   forgotPassword,
-  resetPassword
+  resetPassword,
+  updateDetails,
+  updatePassword
 } = require("../controllers/AuthController");
 
 // Protect route middleware
@@ -15,6 +17,8 @@ const { protect } = require("../middleware/auth");
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/me", protect, getLoggedUser);
+router.put("/updatedetails", protect, updateDetails);
+router.put("/updatepassword", protect, updatePassword);
 router.post("/forgotpassword", forgotPassword);
 router.put("/resetpassword/:resettoken", resetPassword);
 
